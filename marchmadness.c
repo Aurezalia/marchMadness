bd6bc85247409e508b901f705e3989916af72e75
Jeff <jefferson.bagley@umontana.edu>
2018-03-13 13:58:52 -0600

    Tuesday class update

--- marchmadness.c
+++ marchmadness.c
@@ -1,1 +1,41 @@
 //march madness
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  FILE* inFile = NULL;
+  int numRead = 0;
+  const char *roundOf64[64];
+  const char *roundOf32[32];
+  const char *roundOf16[16];
+  const char *roundOf8[8];
+  const char *roundOf4[4];
+  const char *roundOf2[2];
+
+  int i = 0;
+
+  inFile = fopen("NCAA2018Bracket.txt", "r");
+
+  while (!feof(inFile)) {
+    char* word = (char*)malloc(sizeof(char));
+    fscanf(inFile,"[^\n]", word);
+
+        roundof64[i] = word;
+        ++i;
+
+  }
+
+  fclose(inFile);
+
+  for(i=0; i<64; ++i) {
+    printf("%s\n", roundOf64[i]);
+  }
+
+
+
+
+  return 0;
+}
+//march madness
